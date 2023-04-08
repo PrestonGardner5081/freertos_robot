d7f217a9b1282cb06d99e622425d9d9dc11e70ed
Preston Gardner <pgard09@gmail.com>
2023-04-08 10:28:30 -0600

    working tasking and usb serial printing

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -2,18 +2,36 @@
 #include <task.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
 
 void led_task()
 {
-    const uint LED_PIN = 0;
+    //init wifi module
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+    }
+
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        vTaskDelay(100);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        vTaskDelay(100);
+    }
+}
+
+void input_task(){
+
+    const uint LED_PIN = 2;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+
     while (true) {
+        printf("input task running \n");
         gpio_put(LED_PIN, 1);
-        vTaskDelay(100);
+        vTaskDelay(250);
         gpio_put(LED_PIN, 0);
-        vTaskDelay(100);
+        vTaskDelay(250);
     }
 }
 
@@ -22,6 +40,7 @@ int main()
     stdio_init_all();
 
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(input_task, "Input_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
