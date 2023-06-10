53d876627509e4610ad9690dd84e9bf1df3f14ef
preston gardner <ptg5081@psu.edu>
2023-06-10 14:00:58 -0600

    added stuff for debugging

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -5,6 +5,7 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "pico_bt_connection.h"
+#include "hardware/pwm.h"
 
 int picow_bt_init(void) {
     // initialize CYW43 driver architecture (will enable BT if/because CYW43_ENABLE_BLUETOOTH == 1)
@@ -26,17 +27,52 @@ void led_task()
     }
 }
 
-void input_task(){
+/*! \brief Initialise a set of GPIO pins for PWM
+ *
+ * \param pins array of pin numbers 
+ * \param num_pins length of pins
+ */
+void pwm_setup(int *pins, int num_pins){
+    for(int i = 0; i < num_pins; i++){
+        gpio_set_function(pins[i], GPIO_FUNC_PWM);
+    }
+}
 
-    const uint LED_PIN = 2;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+void pwm_task(){
+    const uint motor_driver_pin_B = 2;
+    
+    //num cycles to split pwm into
+    uint16_t max_brightness = 65535;
+
+    // Tell GPIO pin it is allocated to PWM
+    gpio_set_function(motor_driver_pin_B, GPIO_FUNC_PWM);
+
+    // Find out which PWM slice is connected to motor_driver_pin_B
+    uint motor_pin_b_slice_num = pwm_gpio_to_slice_num(motor_driver_pin_B);
+    uint motor_pin_b_channel = pwm_gpio_to_channel(motor_driver_pin_B);
+
+    // Set period of max_brightness cycles (0 to (max_brightness - 1) inclusive)
+    // pwm_set_wrap(motor_pin_b_slice_num, max_brightness - 1);
+
+    // init pwm to level 0
+    pwm_set_gpio_level(motor_driver_pin_B, 4);
+    
+    // Start the PWM 
+    pwm_config config = pwm_get_default_config();
+    pwm_init(motor_pin_b_slice_num, &config, true);
+
 
     while (true) {
-        gpio_put(LED_PIN, 1);
-        vTaskDelay(250);
-        gpio_put(LED_PIN, 0);
-        vTaskDelay(250);
+        // Fade up from 0 -> 100 percent 
+        for(int i = 0; i < max_brightness; i++){
+            pwm_set_gpio_level(motor_driver_pin_B, i);
+            sleep_us(50);  
+        }      
+        // Fade down from 100 -> 0 percent 
+        for(int i = max_brightness - 1; i >= 0; i--){
+            pwm_set_gpio_level(motor_driver_pin_B, i);
+            sleep_us(50);  
+        }     
     }
 }
 
@@ -48,7 +84,7 @@ int main()
     spp_service_setup();
 
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
-    xTaskCreate(input_task, "Input_Task", 256, NULL, 1, NULL);
+    xTaskCreate(pwm_task, "Input_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
