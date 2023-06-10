e49ba38726bb2f85d2db65cde0b3fffb62976b28
preston gardner <ptg5081@psu.edu>
2023-06-10 14:19:11 -0600

    send executable to pico over picoprobe

---
+++ send_to_pico.bat
@@ -0,0 +1,1 @@
+openocd -f interface/cmsis-dap.cfg -c "adapter speed 5000" -f target/rp2040.cfg -s "C:/Users/ptg10/Documents/all_projects/pico_car/openocd-0.12.0-rc2+dev-g9d92577-202211162039-x86/scripts" -c "program C:/Users/ptg10/Documents/all_projects/pico_car/freertos_robot/build/ProjectFiles/rtos_rc.elf verify reset exit"
